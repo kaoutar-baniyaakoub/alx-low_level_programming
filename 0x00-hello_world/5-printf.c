a3c9193238d42022a2ef34f602e78d575f54811f
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-04-20 11:28:11 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
 * main - a program that prints a line with printf function 
 *
-* Return:  0 (Success)
+* Return:0(Success)
 */
-int main (void)
+int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
 }
