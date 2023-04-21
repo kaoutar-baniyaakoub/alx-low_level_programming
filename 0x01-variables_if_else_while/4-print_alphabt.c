d4f94231acc0e7426e7a1e732e812d5f747f60cf
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-04-21 10:58:41 -0700

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+*main-prints alphabets in lowercase except q and e
+*Return:0(Success)
+*/
+int main(void)
+{
+int n = 97;
+if (n !113 || n !101; n++)
+{
+while (n <= 122)
+{
+putchar(n);
+}
+putchar('\n');
+}
+return (0);
+}
