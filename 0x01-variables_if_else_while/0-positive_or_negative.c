194f9d54c4e4596caaa4d6386457167a92547af0
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-04-21 09:34:32 -0700

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include<stdio.h>
-/*
-* main - positive negative zero 
-*Return: Always 0 (Success)
+/**
+*main - positive_negative_zero 
+*Return:0(Success)
 */
 int main(void)
 {
