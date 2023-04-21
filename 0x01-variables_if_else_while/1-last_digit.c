604f0f02e4174979626b5809d9c906674c873cf7
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-04-21 10:03:40 -0700

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ n = rand() - RAND_MAX / 2;
 ld = n % 10;
 if (ld > 5)
 {
-printf("Last digit of %d is %d and is greater than 5 ", n, ld);
+printf("Last digit of %d is %d and is greater than 5", n, ld);
 }
 else if (ld == 0)
 {
