9e5fc954f8269bc44857f32c7689326b7371c3b9
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-04-21 10:08:36 -0700

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ printf("Last digit of %d is %d and is greater than 5", n, ld);
 }
 else if (ld == 0)
 {
-printf("Last digit of %d is %d and is 0", n, ld);
+printf("Last digit of %d is %d and is 0 ", n, ld);
 }
 else
 {
