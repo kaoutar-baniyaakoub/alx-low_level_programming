819e9f21c11f0b959bb1c199adc17e2b54e5960e
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-04-21 11:32:19 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+*main-that prints the lowercase alphabet in reverse
+*Return:0(Success)
+*/
+int main(void)
+{
+int n = 122;
+while (n >= 97)
+{
+putchar(n);
+n--;
+}
+putchar('\n');
+return (0);
+}
