74543f8304957793774be3f2a8cd7da3289698b2
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-04-21 11:39:43 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+*main-program that prints all the numbers of base 16 in lowercase
+*Return:0(Success)
+*/
+int main(void)
+{
+int n;
+for (n = 0; n < 16; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
