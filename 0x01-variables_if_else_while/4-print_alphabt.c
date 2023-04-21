788d6f51f73b5d2efada976d7dc59966cc0c26df
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-04-21 11:02:30 -0700

    task4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,13 +6,16 @@
 int main(void)
 {
 int n = 97;
-if (n !113 || n !101; n++)
-{
 while (n <= 122)
 {
+if (n !113 || n !101)
+{
+n++;
+continue;
+}
 putchar(n);
+n++;
 }
 putchar('\n');
-}
 return (0);
 }
