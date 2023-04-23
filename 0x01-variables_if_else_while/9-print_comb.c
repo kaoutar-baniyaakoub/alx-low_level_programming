609fdf50bf3107b228e3931aa60c2c9e424b57d5
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-04-23 07:59:47 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main-prints all possible combinations of single-digit numbers.
+*Return:0(Success)
+*/
+int main(void)
+{
+int n;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+if (n == 57)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+}
