898a8dd47675fa5e79c18c39d8181ffa22ab32f8
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-04-25 12:07:28 -0700

    task0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*main- prints the alphabet, in lowercase
+*Return:0(Success)
+*/
+void print_alphabet(void)
+{
+int n;
+for (n = 97; n <= 122; n++)
+{
+	_putchar(n);
+}
+_putchar('\n');
+return (0);
+}
