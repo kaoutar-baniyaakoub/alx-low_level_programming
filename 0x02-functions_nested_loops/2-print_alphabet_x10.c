84e2776d1423b17fb6952d0c1f4beef82575f57d
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-04-25 13:41:11 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-*print_alphabet_x10-prints 10 times the alphabet, in lowercase 
+*print_alphabet_x10-prints 10 times the alphabet in lowercase 
 *Return:0(Success)
 */
 void print_alphabet_x10(void)
 {
 int n;
 int m;
-for (m = 1; n <= 10; n++)
+for (n = 1; n <= 10; n++)
 {
 for (m = 97; m <= 122; m++)
 {
