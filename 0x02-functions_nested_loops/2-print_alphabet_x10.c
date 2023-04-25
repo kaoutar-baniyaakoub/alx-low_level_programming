d55cc7e02bb894a264f3e42b98eab02c9d9b1e3f
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-04-25 13:31:40 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_alphabet_x10-prints 10 times the alphabet, in lowercase 
+*Return:0(Success)
+*/
+void print_alphabet_x10(void)
+{
+int n;
+int m;
+for (m = 1; n <= 10; n++)
+{
+for (m = 97; m <= 122; m++)
+{
+_putchar(m);
+}
+}
+_putchar('\n');
+}
