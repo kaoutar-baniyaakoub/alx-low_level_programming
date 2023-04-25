b4b37b5ae65dc1297afddbd066a24405833004ce
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-04-25 12:50:53 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,9 +3,8 @@
 *main-prints the alphabet in lowercase
 *Return:0(Success)
 */
-int main(void)
+void print_alphabet(void)
 {
-void print_alphabet(void);
 int n;
 for (n = 97; n <= 122; n++)
 	_putchar(n);
