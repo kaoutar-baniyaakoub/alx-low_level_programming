3788c3b11aecec1c75a677b03704dfd1b9273c1f
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-04-26 08:21:00 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_abs-coputes the absolute value of an integer
+ *@c:the number to be computed
+ *Return:absolute vlue of number or zero
+ */
+int _abs(int c)
+{
+if (c < 0)
+{
+int abs_val;
+abs_val = c * -1;
+return (abs_val);
+}
+return (c);
+}
