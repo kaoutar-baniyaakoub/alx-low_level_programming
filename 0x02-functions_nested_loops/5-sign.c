70f83ad8da26cf2f686a6891dfc71e26f03bc50b
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-04-26 07:07:26 -0700

    task5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_sign-prints the sign of a number
+*Return:1 if its positive , -1 if its negative and 0 if it is zero
+*/
+int print_sign(int n)
+{
+if (n >= 0)
+{
+_putchar('+');
+return (1);
+}
+if (n == 0)
+{
+_putchar('0');
+return(0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
