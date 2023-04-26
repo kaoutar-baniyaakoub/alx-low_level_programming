6fbd5c30f9056a900f9a17abe03e9574faaa139f
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-04-26 14:39:18 -0700

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add-adds two integers and returns the result
+ *@a:the first number to be computed
+ *@b:the second number to be computed
+ * Return:int value
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
