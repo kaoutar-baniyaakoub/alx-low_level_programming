7d92a998c8a19c06deef123629b2d56045afc75a
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-05-02 07:42:49 -0700

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int- swaps the values of two integers
+ * @b:int to swap
+ * @a:int to swap
+ */
+void swap_int(int *a, int *b)
+{
+int s = *a;
+*a = *b;
+*b = m;
+
+}
