da4ce89b67dc049a782ff831e0724c2e611d7429
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-05-02 07:02:01 -0700

    task0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * reset_to_98-akes a pointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98-akes a pointer to an int as parameter and
+ * updates the value it points to to 98
  * @n:input
  * Return : n
  */
