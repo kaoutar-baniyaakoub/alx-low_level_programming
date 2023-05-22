85a90cecb4c743114349c55c985bb8a67cde6652
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-05-22 15:24:50 -0700

    task4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,30 +2,24 @@
 #define DOG_H
 
 /**
- * struct dog - a dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
- *
- * Description: Longer description
+ * struct dog - struct
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+ * Description: poppy
  */
+
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+char *name;
+float age;
+char *owner;
 };
 
-/**
- * dog_t - typedef for struct dog
- */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-
 #endif
