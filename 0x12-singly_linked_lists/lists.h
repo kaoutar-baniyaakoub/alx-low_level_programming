14617418d02eea486f46d36c27d17edf2d0dce7f
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-06-01 15:54:10 -0700

    wa layrhm bak work

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,6 +20,8 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
+typedef unsigned long size_t;
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
