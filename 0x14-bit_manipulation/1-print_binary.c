b5fc2a26f359e09300970e06b8747bdd27e5411c
kaoutar-baniyaakoub <kaoutar.baniyaakoub@etu.uae.ac.ma>
2023-06-08 14:22:20 -0700

    task one

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_binary - function that prints the binary representation of a number
+ *@n : number to converte
+ * Return:-
+ */
+
+void print_binary(unsigned long int n)
+{
+if (n > 1)
+{
+print_binary(n >> 1);
+}
+putchar('0' + (n & 1));
+}
